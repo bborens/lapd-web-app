45a12b2e1136e3290a2ac3edcd73ddcbd5147fcf
Bryan Borenstein <bborens@wgu.edu>
2019-07-24 16:29:24 -0700

    fixed

--- app.py
+++ app.py
@@ -538,5 +538,5 @@ def update_graph(datePicked, selectedData, selectedLocation):
     )
 
 
-if __name__ == "__main__":
-    app.run_server(debug=True)
+##if __name__ == "__main__":
+  ##  app.run_server(debug=True)
